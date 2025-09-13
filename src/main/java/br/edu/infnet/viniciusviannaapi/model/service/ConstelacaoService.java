94b7474724b98fd96220826537e32ba7807f00d1
vinicius vianna <vnvianna@icloud.com>
2025-09-13 16:40:01 -0300

    RED: testes escritos que falham

--- src/main/java/br/edu/infnet/viniciusviannaapi/model/service/ConstelacaoService.java
+++ src/main/java/br/edu/infnet/viniciusviannaapi/model/service/ConstelacaoService.java
@@ -1,7 +1,24 @@
 package br.edu.infnet.viniciusviannaapi.model.service;
 
+import java.time.Month;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import br.edu.infnet.viniciusviannaapi.model.domain.Constelacao;
 
 @Service
 public class ConstelacaoService {
+
+    public List<Constelacao> obterConstelacoesPorMes(Month mes) {
+        throw new UnsupportedOperationException("Método não implementado");
+    }
+
+    public List<Constelacao> obterConstelacoesVisiveisAHumanos() {
+        throw new UnsupportedOperationException("Método não implementado");
+    }
+
+    public List<Constelacao> obterConstelacoesDoZodiaco() {
+        throw new UnsupportedOperationException("Método não implementado");
+    }
 }
---
+++ src/test/java/br/edu/infnet/viniciusviannaapi/ConstelacaoTest.java
@@ -0,0 +1,115 @@
+package br.edu.infnet.viniciusviannaapi;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.edu.infnet.viniciusviannaapi.model.domain.Constelacao;
+import br.edu.infnet.viniciusviannaapi.model.service.ConstelacaoService;
+
+public class ConstelacaoTest {
+
+    private ConstelacaoService constelacaoService;
+    private List<Constelacao> constelacoes = new ArrayList<>();
+    private Constelacao constelacao1;
+    private Constelacao constelacao2;
+    private Constelacao constelacao3;
+
+    @BeforeEach
+    void setUp() {
+        constelacaoService = new ConstelacaoService();
+        constelacao1 = new Constelacao(
+            1L,
+            "Cisne",
+            "Cyg" ,
+            5,
+            true,
+            Set.of(Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER)
+        );
+
+        constelacao2 = new Constelacao(
+            2L,
+            "Andrômeda",
+            "And" ,
+            7,
+            true,
+            Set.of(Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER)
+        );
+
+        constelacao3 = new Constelacao(
+            3L,
+            "Virgem",
+            "Vir",
+            9,
+            true,
+            Set.of(Month.DECEMBER, Month.JANUARY, Month.FEBRUARY)
+        );
+    }
+
+    @Test
+    @DisplayName("Deve retornar constelações visíveis em um mês específico")
+    void deveRetornarConstelacoesVisiveis_EmUmMesEspecifico() {
+        //dado
+        Month mes = Month.OCTOBER;
+        constelacoes.addAll(List.of(constelacao1, constelacao2, constelacao3));
+
+        //quando
+        List<Constelacao> resultado = constelacaoService.obterConstelacoesPorMes(mes);
+
+        //entao
+        assertEquals(2, resultado.size(), "Deveria retornar 2 constelações visíveis em Outubro");
+        assertTrue(resultado.stream().anyMatch(c -> c.getNome().equals("Cisne")), "Deveria conter a constelação Cisne");
+        assertTrue(resultado.stream().anyMatch(c -> c.getNome().equals("Andrômeda")), "Deveria conter a constelação Andrômeda");
+    }
+
+    @Test
+    @DisplayName("Deve retornar apenas constelações do zodíaco")
+    void deveRetornarApenasConstelacoesDoZodiaco() {
+        //dado
+        constelacoes.addAll(List.of(constelacao1, constelacao2, constelacao3));
+
+        //quando
+        List<Constelacao> resultado = constelacaoService.obterConstelacoesDoZodiaco();
+
+        //entao
+        assertEquals(1, resultado.size(), "Deveria retornar 1 constelação do zodíaco");
+        assertTrue(resultado.stream().allMatch(Constelacao::ehDoZodiaco));
+        assertTrue(resultado.stream().anyMatch(c -> c.getNome().equals("Virgem")), "Deveria conter a constelação Virgem");
+    }
+
+    @Test
+    @DisplayName("deve retornar lista vazia quando não há constelação do zodíaco")
+    void deveRetornarListaVazia_quandoNaoHaConstelacaoDoZodiaco() {
+        //dado
+        constelacoes.clear();
+
+        //quando
+        List<Constelacao> resultado = constelacaoService.obterConstelacoesDoZodiaco();
+
+        //entao
+        assertTrue(resultado.isEmpty(), "Deveria retornar lista vazia");
+    }
+
+    @Test
+    @DisplayName("Deve retornar apenas constelações visíveis a humanos")
+    void deveRetornarApenasConstelacoes_quandoVisiveisAHumanos() {
+        //dado
+        constelacoes.addAll(List.of(constelacao1, constelacao2, constelacao3));
+
+        //quando
+        List<Constelacao> resultado = constelacaoService.obterConstelacoesVisiveisAHumanos();
+        
+        //entao
+        assertEquals(3, resultado.size(), "Deveria retornar 3 constelações visíveis a humanos");
+        assertTrue(resultado.stream().allMatch(Constelacao::getVisivelAHumanos));
+    }
+}
